ea7a9d93cc3186a96bc6110120b6692140aa3f2e
joyeuse-cloud <bizimanafabrice707@gmail.com>
2022-02-26 19:14:45 +0200

    edition

--- pillow_module.py
+++ pillow_module.py
@@ -2,27 +2,27 @@ from PIL import Image
 import os, sys
 
 img = Image.open('kjsdnkjs.png')
-# print(img.format)
-# print(img.size)
-# print(img.height)
-# print(img.width)
-# print(img.info)
-# img.show()
+print(img.format)
+print(img.size)
+print(img.height)
+print(img.width)
+print(img.info)
+img.show()
 
 #Convert img to JPEG
 
 jpgimg = img.convert('RGB')
-# jpgimg.save('jpg_converted.jpg')
-# jpgimg.show()
+jpgimg.save('jpg_converted.jpg')
+jpgimg.show()
 
 
 #create thumbnails from JPEG
 
-# conimg = img.convert('RGB')
-# conimg.thumbnail((70, 70))
-# conimg.save('thumbnail.jpg')
-# conimg.show()
+conimg = img.convert('RGB')
+conimg.thumbnail((70, 70))
+conimg.save('thumbnail.jpg')
+conimg.show()
 
 #resize img
-# resizedImg = img.resize((150, 150))
-# resizedImg.show()
+resizedImg = img.resize((150, 150))
+resizedImg.show()
